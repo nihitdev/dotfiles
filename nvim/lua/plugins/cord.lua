9162daf165c0f9609d38f2ea3a749adaa515430b
Nihit Sunhare <code.nihit.dev@gmail.com>
2026-08-17 18:08:01 +0530

    add Neovim Discord presence

---
+++ nvim/lua/plugins/cord.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "vyfor/cord.nvim",
+    event = "VeryLazy",
+    opts = {
+      editor = {
+        client = "neovim",
+        tooltip = "Neovim",
+      },
+      display = {
+        theme = "catppuccin",
+        flavor = "dark",
+        swap_icons = true,
+      },
+    },
+  },
+}
